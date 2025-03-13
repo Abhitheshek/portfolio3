1f04ac5122428ef5d402cc9a2313eca149ab97f3
Abhishek9140yadav <161363963+Abhishek9140yadav@users.noreply.github.com>
2025-03-14 03:58:30 +0530

    Your commit message describing the changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { terser } from '@rollup/plugin-terser'
 
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600, // Increase the warning limit (default is 500 KB)
     rollupOptions: {
+      plugins: [terser()], // Ensure the correct Rollup plugin is used
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
